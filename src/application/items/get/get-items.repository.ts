add3f3534aefbf7aa9ebbe0fbfa8a6fa1664c733
Lothering0 <danil2005232@gmail.com>
2024-10-21 19:49:53 +0300

    feat(items): caching

--- src/application/items/get/get-items.repository.ts
+++ src/application/items/get/get-items.repository.ts
@@ -1,13 +1,31 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { Sql } from 'postgres';
 import { POSTGRES_INJECTION_TOKEN } from 'src/infrastructure/postgres/postgres.injection-token';
 import { Item } from 'src/models/items/item.model';
 
 @Injectable()
 export class GetItemsRepository {
-  constructor(@Inject(POSTGRES_INJECTION_TOKEN) private readonly sql: Sql) {}
+  constructor(
+    @Inject(POSTGRES_INJECTION_TOKEN)
+    private readonly sql: Sql,
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {}
 
-  getMany() {
-    return this.sql<Item[]>`SELECT * FROM items`;
+  async getMany() {
+    /** Caching time. 5 minutes */
+    const TTL = 1000 * 60 * 5;
+    const CACHE_KEY = 'items';
+
+    const cachedData = await this.cacheManager.get(CACHE_KEY);
+
+    if (cachedData) return cachedData;
+
+    const data = await this.sql<Item[]>`SELECT * FROM items`;
+
+    this.cacheManager.set(CACHE_KEY, data, TTL);
+    return data;
   }
 }
